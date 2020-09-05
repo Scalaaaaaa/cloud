b390608d0b883052a791da569a3c01ee12cc9b69
yiyun <1419122914@qq.com>
2020-09-05 13:25:18 +0800

    init

---
+++ auth/src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -0,0 +1,75 @@
+package com.example.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collection;
+/**
+ * @Description 基本web安全配置
+ * @Author yiyun
+ * @Date 2020/9/5
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(s -> new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return AuthorityUtils.createAuthorityList("USER");
+            }
+
+            @Override
+            public String getPassword() {
+                return passwordEncoder().encode("my-password");
+            }
+
+            @Override
+            public String getUsername() {
+                return "my-username";
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        });
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ gateway/src/main/java/com/example/gateway/com/example/gateway/ResourceServerConfig.java
@@ -0,0 +1,38 @@
+package com.example.gateway.com.example.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @AUthor yiyun
+ * @Date 2020-09-04
+ * @Description 网关用来转发和  判断是否有权限转发
+ * 转发：在配置文件配置转发规则
+ * 是否有权限转发：通过约定的/auth/token接口判断入参中的token是否有效，所以 /auth/token一定会配置在转发规则
+ */
+@EnableWebFluxSecurity
+public class ResourceServerConfig {
+    /***
+     * @Description WebFluxSecurity关键配置-过滤器，过滤掉没权限的请求
+     * @Author yiyun
+     * @Date 2020/9/4
+     */
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        // 虽然这里只开放了 /actuator/**  但是，实际上，/auth/××的也开放了。至少/auth/token的开放了，代测试其他的是否开放
+        http.authorizeExchange()
+                .pathMatchers("/actuator/**").permitAll()
+                // 如果是细节的判断，则代码是.anyExchange().access(authorizationManager)
+                // 下面的.anyExchange().authenticated()表示  只要jwt认证通过，则有权限访问所有资源（接口）
+                //TODO 后续增加RBAC（Role Based Access Control)控制资源 像第三方应用获取微信头像信息这种需求
+                // 可以用jwt的有效时间字段 来控制
+                .anyExchange().authenticated();
+        // 开启oauth2的资源服务器  和  jwt支持
+        // 简单理解，jwt就是在你登陆的时候给你一个令牌，请求资源时带上令牌  后台就能判断你是你
+        // oauth2就是  根据jwt令牌里的附加信息（比如角色）来判断你有没有权限访问该资源
+        http.oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
